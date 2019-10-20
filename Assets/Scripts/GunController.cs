a6f25c653ad89d7c06b13695a24678736654bc39
KleptoKat76 <patrickzhang2578@gmail.com>
2019-10-20 15:11:58 -0500

    more stuff

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -8,9 +8,12 @@ public class GunController : MonoBehaviour
     private float timer;
     private Transform nozzle;
     public GameObject projectile;
+
+    private ControlScheme controlscheme;
     // Start is called before the first frame update
     void Start()
     {
+        controlscheme = transform.parent.GetComponent<PlayerController>().getControlScheme();
         nozzle = transform.GetChild(0);
         timer = shootCooldown;
     }
@@ -18,17 +21,27 @@ public class GunController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Plane plane = new Plane(Vector3.forward, new Vector3(Camera.main.transform.position.x,
-                                                        Camera.main.transform.position.y, 0));
-        float enter = 0.0f;
-        if (plane.Raycast(ray, out enter))
+        if (controlscheme.GunAimXAxis.Contains("mouse"))
         {
-            Vector3 hitPoint = ray.GetPoint(enter);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Plane plane = new Plane(Vector3.forward, new Vector3(Camera.main.transform.position.x,
+                                                            Camera.main.transform.position.y, 0));
+            float enter = 0.0f;
+            if (plane.Raycast(ray, out enter))
+            {
+                Vector3 hitPoint = ray.GetPoint(enter);
+            }
+            var mousePos = ray.GetPoint(enter);
+            var direction = mousePos - transform.position;
+            transform.up = direction;
         }
-        var mousePos = ray.GetPoint(enter);
-        var direction = mousePos - transform.position;
-        transform.up = direction;
+        else
+        {
+            if (!(Input.GetAxis(controlscheme.GunAimXAxis) == 0 && Input.GetAxis(controlscheme.GunAimYAxis) == 0)){
+                transform.up = new Vector2(Input.GetAxis(controlscheme.GunAimXAxis), Input.GetAxis(controlscheme.GunAimYAxis));
+            }
+        }
     }
     private void FixedUpdate()
     {
---
+++ Assets/Scripts/PlayerGameState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGameState : MonoBehaviour
+{
+    public enum PlayerID
+    {
+        p1, p2, p3, p4
+    }
+    public enum State
+    {
+        dead, alive
+    }
+    private PlayerID playerID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
